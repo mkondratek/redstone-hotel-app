60bab6e6066ae2cd9203d8229975e1d429ba9627
Mikołaj Kondratek <mik.kondratek@gmail.com>
2020-01-05 13:52:20 +0100

    Initial commit

---
+++ src/main/java/com/example/redstonehotelapp/RedstoneHotelAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.redstonehotelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedstoneHotelAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedstoneHotelAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/redstonehotelapp/RedstoneHotelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redstonehotelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedstoneHotelAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
