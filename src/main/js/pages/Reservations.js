56bb3b63c1c1b7b98b1798250de5b04e633368a8
Mikołaj Kondratek <mik.kondratek@gmail.com>
2020-01-12 20:45:55 +0100

    add reservation form with awesome date range picker

--- src/main/js/pages/Reservations.js
+++ src/main/js/pages/Reservations.js
@@ -1,22 +1,78 @@
 import React, {Component} from "react";
-import {Parallax} from "react-scroll-parallax/cjs";
 import {RegularParallax} from "../util/RegularParallax";
+import DatePicker from "react-datepicker";
+
+import "../../../../node_modules/react-datepicker/dist/react-datepicker.css";
 
 class Reservations extends Component {
+    constructor(props) {
+        super(props);
+        let now = new Date();
+        this.state = {
+            startDate: now,
+            endDate: now
+        }
+    }
+
+    setStartDate(date) {
+        const newEndDate = this.state.endDate < date ? date : this.state.endDate;
+        this.setState({startDate: date, endDate: newEndDate});
+    }
+
+    setEndDate(date) {
+        const newStartDate = this.state.startDate > date ? date : this.state.startDate;
+        this.setState({startDate: newStartDate, endDate: date});
+    }
+
     render() {
+        const {startDate, endDate} = this.state;
         return (
             <div>
                 <RegularParallax src="parallaxImgs/Enchant.png"/>
                 <div className="content">
                     <h2>Reservations</h2>
-                    <p>Cras facilisis urna ornare ex volutpat, et
-                        convallis erat elementum. Ut aliquam, ipsum vitae
-                        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-                        metus nec massa. Maecenas hendrerit laoreet augue
-                        nec molestie. Cum sociis natoque penatibus et magnis
-                        dis parturient montes, nascetur ridiculus mus.</p>
+                    <p>Book your stay at the Redstone Hotel. Spend your nights safely from creepers and zombies. This is
+                        an ideal place to gather the team for further journey. Our professional service is ready to
+                        serve you. <i>Reserve now.</i></p>
 
-                    <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+                    <form>
+                        <label>
+                            Name: <input type="text" name="name"/>
+                        </label><br/>
+                        <label>
+                            Surname: <input type="text" name="surname"/>
+                        </label><br/>
+                        <label>
+                            Room: <select>
+                            <option value="ra">Room A</option>
+                            <option value="rb">Room B</option>
+                            <option value="rc">Room C</option>
+                            <option value="rd">Room D</option>
+                        </select>
+                        </label><br/>
+                        <label>
+                            Date:
+                            <DatePicker
+                                selected={startDate}
+                                onChange={date => this.setStartDate(date)}
+                                selectsStart
+                                startDate={startDate}
+                                endDate={endDate}
+                                minDate={Date.now()}
+                            />
+                            <DatePicker
+                                selected={endDate}
+                                onChange={date => this.setEndDate(date)}
+                                selectsEnd
+                                startDate={startDate}
+                                endDate={endDate}
+                                minDate={startDate}
+                            />
+                        </label><br/>
+                        <input type="submit" value="Reserve"/>
+                    </form>
+
+                    <p>We look forward to your presence.</p>
                 </div>
                 <RegularParallax src="parallaxImgs/Billard.png"/>
             </div>
